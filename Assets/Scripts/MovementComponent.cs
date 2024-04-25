1a387ef980c69b2750934c5a5d9c65c589139699
Neto <100791626+mneet@users.noreply.github.com>
2024-04-24 22:11:02 -0300

    Update MovementComponent.cs

--- Assets/Scripts/MovementComponent.cs
+++ Assets/Scripts/MovementComponent.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class MovementComponent : MonoBehaviour
 {
+    [SerializeField] bool rotateBehaviour = false;
+    [SerializeField] Vector3 rotateDirection;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float moveSpeed;
+
+    private enum MovementDirection {
+        HORIZONTAL,
+        VERTICAL,
+        RANDOM
+    }
+    [SerializeField] private MovementDirection movementDirection;
 
     // Update is called once per frame
     void Update()
